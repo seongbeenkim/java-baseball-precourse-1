c85cb253c2ee7ebd9bef78f1ce41513e3264ed49
Beenie93 <seongbeen93@naver.com>
2021-10-03 04:56:52 +0900

    feat: 공 집합 객체 생성 및 생성 시 검증 기능 구현

---
+++ src/main/java/baseball/domain/ball/Balls.java
@@ -0,0 +1,62 @@
+package baseball.domain.ball;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class Balls {
+
+    private static final int STANDARD_SIZE = 3;
+
+    private final Set<Ball> balls;
+
+    public Balls(final List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+        this.balls = createBallsFrom(numbers);
+    }
+
+
+    private void validateSize(final List<Integer> numbers) {
+        if (isInvalidSize(numbers)) {
+            throw new IllegalArgumentException(String.format("총 %d개의 숫자가 존재해야합니다", STANDARD_SIZE));
+        }
+    }
+
+    private boolean isInvalidSize(final Collection<Integer> numbers) {
+        return STANDARD_SIZE != numbers.size();
+    }
+
+    private void validateDuplication(final List<Integer> numbers) {
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (isInvalidSize(distinctNumbers)) {
+            throw new IllegalArgumentException("서로 다른 수로 구성되어야합니다.");
+        }
+    }
+
+    private Set<Ball> createBallsFrom(final List<Integer> numbers) {
+        Set<Ball> balls = new HashSet<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            Ball ball = new Ball(i, numbers.get(i));
+            balls.add(ball);
+        }
+
+        return balls;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Balls balls1 = (Balls) o;
+        return Objects.equals(balls, balls1.balls);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(balls);
+    }
+}
---
+++ src/test/java/baseball/domain/ball/BallsTest.java
@@ -0,0 +1,60 @@
+package baseball.domain.ball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class BallsTest {
+
+    @Test
+    @DisplayName("숫자 목록를 입력받아 공 집합 객체를 생성한다.")
+    void create() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+
+        //when
+        Balls balls = new Balls(numbers);
+
+        //then
+        assertThat(balls).isEqualTo(new Balls(numbers));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2", "1, 2, 3, 4"})
+    @DisplayName("3개의 숫자가 아닐 경우, 예외가 발생한다.")
+    void create_fail_with_invalid_size(String input) {
+        //given
+        String[] splitNumbers = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String splitNumber : splitNumbers) {
+            int number = Integer.parseInt(splitNumber);
+            numbers.add(number);
+        }
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Balls(numbers))
+                .withMessage("총 3개의 숫자가 존재해야합니다");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 1, 2", "1, 2, 2", "1, 2, 1"})
+    @DisplayName("중복된 숫자가 존재할 경우, 예외가 발생한다.")
+    void create_fail_with_duplicated_numbers(int number1, int number2, int number3) {
+        //given
+        List<Integer> numbers = Arrays.asList(number1, number2, number3);
+
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Balls(numbers))
+                .withMessage("서로 다른 수로 구성되어야합니다.");
+    }
+}
