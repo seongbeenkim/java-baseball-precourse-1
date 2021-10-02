73ef11cc1338499900022e763c4ce51bd746883f
Beenie93 <seongbeen93@naver.com>
2021-10-02 15:12:31 +0900

    test : Set의 값이 포함될 경우 참을 반환하는 contains 학습 테스트 구현

--- src/test/java/nextstep/test/study/SetTest.java
+++ src/test/java/nextstep/test/study/SetTest.java
@@ -3,6 +3,8 @@ package nextstep.test.study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,5 +32,16 @@ public class SetTest {
 
         //then
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("값이 포함될 경우 참을 반환한다.")
+    void contains(int value) {
+        //when
+        boolean isContained = numbers.contains(value);
+
+        //then
+        assertThat(isContained).isTrue();
     }
 }
