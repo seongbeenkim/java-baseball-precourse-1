e84e7249e5d4b8b0a2efc0b560fdb3ff088501ec
Beenie93 <seongbeen93@naver.com>
2021-10-03 20:48:38 +0900

    feat : 비교하여 얻은 공의 상태를 추가하는 기능 및 테스트 구현

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -5,6 +5,10 @@ public class Result {
     private final int strikeCount;
     private final int ballCount;
 
+    public Result() {
+        this(0, 0);
+    }
+
     public Result(final int strikeCount, final int ballCount) {
         this.strikeCount = strikeCount;
         this.ballCount = ballCount;
@@ -12,5 +16,25 @@ public class Result {
 
     public boolean isAllStrike() {
         return strikeCount == 3;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public Result add(final Status status) {
+        if (status.isStrike()) {
+            return new Result(strikeCount + 1, ballCount);
+        }
+
+        if (status.isBall()) {
+            return new Result(strikeCount, ballCount + 1);
+        }
+
+        return this;
     }
 }
--- src/test/java/baseball/domain/ResultTest.java
+++ src/test/java/baseball/domain/ResultTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ResultTest {
 
@@ -20,5 +21,22 @@ public class ResultTest {
 
         //then
         assertThat(isAllStrike).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"STRIKE, 1, 0", "BALL, 0, 1"})
+    @DisplayName("전달받은 상태를 추가한 새로운 결과 객체를 반환한다.")
+    void add(Status status, int strikeCount, int ballCount) {
+        //given
+        Result result = new Result();
+
+        //when
+        Result addedResult = result.add(status);
+
+        //then
+        assertAll(
+                () -> assertThat(addedResult.getStrikeCount()).isEqualTo(strikeCount),
+                () -> assertThat(addedResult.getBallCount()).isEqualTo(ballCount)
+        );
     }
 }
