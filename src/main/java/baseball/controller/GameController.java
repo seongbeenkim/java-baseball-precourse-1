090822c2097a301bc3d202170d6791ad2c51326a
Beenie93 <seongbeen93@naver.com>
2021-10-05 13:18:13 +0900

    feat : 게임이 실행되는 Controller 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,66 @@
+package baseball.controller;
+
+import baseball.domain.Balls;
+import baseball.domain.Command;
+import baseball.domain.Computer;
+import baseball.domain.Result;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        Computer computer = new Computer();
+        Result result;
+        do {
+            Balls userBalls = createUserBalls();
+            result = computer.match(userBalls);
+            outputView.printResult(result);
+        } while (result.isNotThreeStrike());
+
+        outputView.printGameEndingMessage();
+        decideToReStart();
+    }
+
+    private Balls createUserBalls() {
+        try {
+            List<Integer> ballNumbers = inputView.getBallNumbers();
+            return new Balls(ballNumbers);
+        } catch (NoSuchElementException e) {
+            outputView.printError(e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            outputView.printError(e.getMessage());
+            return createUserBalls();
+        }
+    }
+
+    private void decideToReStart() {
+        Command command = getCommand();
+
+        if (command.isStart()) {
+            run();
+        }
+    }
+
+    private Command getCommand() {
+        try {
+            int commandNumber = inputView.getCommandNumber();
+            return Command.findByNumber(commandNumber);
+        } catch (Exception e) {
+            outputView.printError(e.getMessage());
+            return getCommand();
+        }
+    }
+}
